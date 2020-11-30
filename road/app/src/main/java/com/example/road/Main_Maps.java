f57b201b3c7e229742bc7b640376df13cac3ad8b
RyuSw <rsw1452@naver.com>
2020-11-30 23:18:52 +0900

    현재위치, 맵

---
+++ road/app/src/main/java/com/example/road/Main_Maps.java
@@ -0,0 +1,21 @@
+package com.example.road;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Main_Maps extends FragmentActivity implements OnMapReadyCallback {
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
